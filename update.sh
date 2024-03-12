12ea8de690974c0be05200950f86b1378f1a189d
razonyang <razonyang@gmail.com>
2024-03-12 19:16:16 +0800

    chore(deps): update modules

--- update.sh
+++ update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-docker compose run base hugo mod get -u & \
-docker compose run blog hugo mod get -u & \
-docker compose run bootstrap hugo mod get -u & \
-docker compose run common hugo mod get -u & \
-docker compose run decap-cms hugo mod get -u & \
-docker compose run docker hugo mod get -u & \
-docker compose run echarts hugo mod get -u & \
-docker compose run home hugo mod get -u & \
-docker compose run hugopress hugo mod get -u & \
-docker compose run icons hugo mod get -u & \
-docker compose run images hugo mod get -u & \
-docker compose run kroki hugo mod get -u & \
-docker compose run pwa hugo mod get -u & \
-docker compose run search hugo mod get -u & \
-docker compose run seo hugo mod get -u & \
-docker compose run shortcodes hugo mod get -u
+docker compose run base hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run blog hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run bootstrap hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run common hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run decap-cms hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run docker hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run echarts hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run home hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run hugopress hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run icons hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run images hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run kroki hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run pwa hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run search hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run seo hugo mod get -u github.com/hbstack/base@v0.4.5 & \
+docker compose run shortcodes hugo mod get -u github.com/hbstack/base@v0.4.5
