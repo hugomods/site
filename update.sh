442494ef1688af3abd67aa49a3c2037101edb9ed
razonyang <razonyang@gmail.com>
2024-03-21 22:30:37 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 docker compose run base hugo mod get -u github.com/hbstack/base@v0.4.5 & \
 docker compose run blog hugo mod get -u github.com/hbstack/base@v0.4.5 & \
 docker compose run bootstrap hugo mod get -u github.com/hbstack/base@v0.4.5 & \
-docker compose run common hugo mod get -u github.com/hbstack/base@v0.4.5 & \
 docker compose run decap-cms hugo mod get -u github.com/hbstack/base@v0.4.5 & \
 docker compose run docker hugo mod get -u github.com/hbstack/base@v0.4.5 & \
 docker compose run echarts hugo mod get -u github.com/hbstack/base@v0.4.5 & \
